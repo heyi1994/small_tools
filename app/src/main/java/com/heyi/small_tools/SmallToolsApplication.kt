ccb36380d80c30095ef9690c650a40ff7a8b6079
heyidada <1875244467@qq.com>
2018-03-01 12:31:34 +0800

    :tada: Initial commit and add skin manager.

---
+++ app/src/main/java/com/heyi/small_tools/SmallToolsApplication.kt
@@ -0,0 +1,39 @@
+package com.heyi.small_tools
+
+import android.app.Application
+import android.content.Context
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.support.multidex.MultiDex
+
+/**
+ * @author Heyi
+ * @since 1.0.0
+ */
+class SmallToolsApplication:Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        if (newConfig?.fontScale!=1f){
+            getResources()
+        }
+        super.onConfigurationChanged(newConfig)
+    }
+
+    override fun getResources(): Resources {
+        val resources = super.getResources()
+        if (resources.configuration.fontScale!=1f){
+            val configuration = Configuration()
+            configuration.setToDefaults()
+            resources.updateConfiguration(configuration,resources.displayMetrics)
+        }
+        return super.getResources()
+    }
+
+}
